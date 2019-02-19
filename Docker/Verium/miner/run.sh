28ca1e748ea955887612745e20e1fa1146961468
zachary hudson <zconingford@gmail.com>
2019-02-19 03:49:19 -0500

    single thread miner

--- Docker/Verium/miner/run.sh
+++ Docker/Verium/miner/run.sh
@@ -1,4 +1,4 @@
 echo $MINER_ADDR_NAME
 
 
-./cpuminer -o stratum+tcp://pool-eu.bloxstor.com:3003 -u $MINER_ADDR_NAME  -p winner1
+./cpuminer -o stratum+tcp://pool-eu.bloxstor.com:3003 -t 1 -u $MINER_ADDR_NAME  -p winner1
